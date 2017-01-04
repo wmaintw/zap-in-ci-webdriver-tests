bf8bbf7ec87bccee6c51c6752b2d93d1dcdda6cb
wma <wma@thoughtworks.com>
2017-01-04 20:28:36 +0800

    refactor: introduce default baseUrl

--- src/test/intg/java/pages/BasePage.java
+++ src/test/intg/java/pages/BasePage.java
@@ -1,9 +1,16 @@
 package pages;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 public class BasePage {
-    protected String baseUrl = "http://security.local:8282/bodgeit";
+    protected String defaultBaseUrl = "http://security.local:8282/bodgeit";
 
     protected String buildUrl(String path) {
-        return baseUrl + path;
+        return getBaseUrl() + path;
+    }
+
+    private String getBaseUrl() {
+        String baseUrl = System.getProperty("baseUrl");
+        return isEmpty(baseUrl) ? defaultBaseUrl : baseUrl;
     }
 }
