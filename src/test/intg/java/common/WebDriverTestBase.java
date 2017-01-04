cb694fe563c95fa6f24f2144485523a54ce9f2fc
wma <wma@thoughtworks.com>
2017-01-04 17:08:23 +0800

    use embedded geckodriver

--- src/test/intg/java/common/WebDriverTestBase.java
+++ src/test/intg/java/common/WebDriverTestBase.java
@@ -1,25 +1,20 @@
 package common;
 
 import com.google.gson.JsonObject;
-import com.sun.jna.platform.win32.WinBase;
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.Proxy;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class WebDriverTestBase {
     protected FirefoxDriver driver;
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "/Users/wma/develop/common-used-components/geckodriver");
-
         JsonObject proxyJson = new JsonObject();
         proxyJson.addProperty("proxyType", "manual");
         proxyJson.addProperty("httpProxy", "127.0.0.1");
