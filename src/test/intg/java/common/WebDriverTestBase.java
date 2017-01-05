cc28d6a1842ed2d3739ae0505005d72386cc1cc3
wma <wma@thoughtworks.com>
2017-01-05 10:26:28 +0800

    use different geckodriver for different OS

--- src/test/intg/java/common/WebDriverTestBase.java
+++ src/test/intg/java/common/WebDriverTestBase.java
@@ -12,13 +12,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class WebDriverTestBase {
     protected FirefoxDriver driver;
     private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
+    private static final String DEFAULT_GECKODRIVER_ON_MAC = "/Users/wma/develop/security/zap ci/drivers/geckodriver-mac";
 
     @Before
     public void setUp() throws Exception {
 
         String driverPath = System.getProperty(WEBDRIVER_GECKO_DRIVER);
         if (StringUtils.isEmpty(driverPath)) {
-            System.setProperty(WEBDRIVER_GECKO_DRIVER, "/Users/wma/develop/security/zap ci/drivers/geckodriver");
+            System.setProperty(WEBDRIVER_GECKO_DRIVER, DEFAULT_GECKODRIVER_ON_MAC);
         }
 
         System.out.println("*********************************************");
